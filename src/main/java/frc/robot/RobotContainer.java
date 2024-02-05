4410fd018c37db0d6f2c382a06b86e12a3ec137d
Steven <scarmain@gmail.com>
2024-02-04 23:34:18 -0500

    Initial SIM support

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -35,6 +35,10 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.TestShooter;
 import frc.robot.hardware.ShooterHw;
+import frc.robot.interfaces.IIntakeHw;
+import frc.robot.simulation.IntakeSim;
+import frc.robot.simulation.ShooterSim;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.PracticeSwerveHw;
 import frc.robot.subsystems.Shooter;
 
@@ -75,6 +79,8 @@ public class RobotContainer {
             //either buzz or simulation
             swerveDrive = new SwerveDriveTrain(new SwerveDriveSim(), odometry);
             odometry.setGyroHardware(new SimSwerveGyro(swerveDrive));
+            shooter = new Shooter(new ShooterSim());
+            new Intake(new IntakeSim());
         } else if (serNum.equals("031e3219")) {
             //practice robot
             swerveDrive = new SwerveDriveTrain(new PracticeSwerveHw(), odometry);
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,8 +1,23 @@
 package frc.robot.subsystems;
 
-public class Intake {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.interfaces.IIntakeHw;
+
+public class Intake extends SubsystemBase {
+    private IIntakeHw hardware;
+
+    public Intake(IIntakeHw hardware) {
+        super();
+        this.hardware = hardware;
+    }
+
+    @Override
+    public void periodic() {
+        hardware.updateInputs();
+    }
+
     public boolean isPieceDetected() {
-        return false;
+        return hardware.isPieceSeen();
     }
 
     public void runMotor() {
