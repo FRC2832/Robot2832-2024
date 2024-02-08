f7bf6e08a099212a871fc8f8782ac16b5f1719af
Starpilot785 <starpilot785@gmail.com>
2024-02-07 20:02:32 -0500

    More Bug Fixes

--- src/main/java/frc/robot/commands/OperatorStick.java
+++ src/main/java/frc/robot/commands/OperatorStick.java
@@ -22,20 +22,29 @@ public class OperatorStick extends Command {
         addRequirements(intake);
     }
 
+    @Override
     public void initialize() {
         SmartDashboard.putNumber("Intake Command",0);
+        SmartDashboard.putNumber("Shooter Command",0);
     }
 
+    @Override
     public void execute() {
         if(cont.IsSubShotRequested()){
             shoot.setPower(SmartDashboard.getNumber("Shooter Command", 0));
             intake.setPower(SmartDashboard.getNumber("Intake Command", 0));
         }
+        else{
+            shoot.setPower(0);
+            intake.setPower(0);
+        }
     }
-
+    
+    @Override
     public boolean isFinished() {
         return false;
     }
 
+    @Override
     public void end(boolean interrupted) {}
 }
