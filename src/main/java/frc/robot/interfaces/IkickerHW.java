eb393152c0d6995170a664d095bc5d8bfa9b0de6
scarmain <scarmain@gmail.com>
2024-02-08 20:29:34 -0500

    delete more lowercase

--- src/main/java/frc/robot/interfaces/IkickerHW.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.interfaces;
-
-public interface IKickerHw {
-    void setPower(double power);
-    void setRpm(double rpm);
-    void updateInputs();
-    double getCurrentRPM(int shooterID);
-}
