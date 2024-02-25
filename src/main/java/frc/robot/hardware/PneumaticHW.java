f39bf2594226e830991a9f41dc2e265aad154623
scarmain <scarmain@gmail.com>
2024-02-25 00:26:51 -0500

    change the aim pigeon to the right direction

--- src/main/java/frc/robot/hardware/PneumaticHW.java
+++ src/main/java/frc/robot/hardware/PneumaticHW.java
@@ -23,7 +23,7 @@ public class PneumaticHW implements IPneumaticHW {
 
     @Override
     public void updateInputs() {
-        currentAngle = angSensor.getPitch();
+        currentAngle = angSensor.getRoll();
     }
 
     @Override 
