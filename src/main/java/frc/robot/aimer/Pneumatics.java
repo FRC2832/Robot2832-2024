44f2f038ebfc815fbe58241b219378ad086fac89
Starpilot785 <starpilot785@gmail.com>
2024-03-13 21:03:12 -0400

    Added File Reading for shooter tables

--- src/main/java/frc/robot/aimer/Pneumatics.java
+++ src/main/java/frc/robot/aimer/Pneumatics.java
@@ -1,14 +1,25 @@
 package frc.robot.aimer;
 
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Pneumatics extends SubsystemBase {
     IPneumaticHW hardware;
     private double moe = 4.00;
+    private BufferedReader reader = null;
+    private InterpolatingDoubleTreeMap upTable;
 
     public Pneumatics(IPneumaticHW hardware) {
         super();
         this.hardware = hardware;
+        upTable = new InterpolatingDoubleTreeMap();
+        readShooterTable("/Aimer_calibration_up.csv",upTable);
     }
 
     public void goTo(double target) {
@@ -44,5 +55,27 @@ public class Pneumatics extends SubsystemBase {
     @Override
     public void periodic() {
         hardware.updateInputs();
+    }
+
+    public void readShooterTable(String fileName, InterpolatingDoubleTreeMap table){
+        try {
+			String path = Filesystem.getDeployDirectory() + fileName;
+            reader = new BufferedReader(new FileReader(path));
+			String line = reader.readLine();
+
+			while ((line = reader.readLine()) != null) {
+                var parts = line.split(",");
+                if(parts.length < 2){
+                    continue;
+                }
+                double angle = Double.parseDouble(parts[1]);
+                double time = Double.parseDouble(parts[0]);
+                table.put(angle,time);
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
