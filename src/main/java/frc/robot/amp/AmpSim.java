03588436f5af2094fb94d62319156387807133d4
scarmain <scarmain@gmail.com>
2024-03-21 23:36:58 -0400

    Add amp change direction

--- src/main/java/frc/robot/amp/AmpSim.java
+++ src/main/java/frc/robot/amp/AmpSim.java
@@ -1,5 +1,7 @@
 package frc.robot.amp;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
 public class AmpSim implements IAmpHw {
 
     @Override
@@ -8,6 +10,12 @@ public class AmpSim implements IAmpHw {
 
     @Override
     public void SetAmpDirection(boolean up) {
+    }
+
+    @Override
+    public Value getDirection() {
+        // TODO Auto-generated method stub
+        return Value.kOff;
     }
     
 }
