024d9603e6e7f33459b53ecbf935329d1d63f891
Steven <scarmain@gmail.com>
2024-07-30 18:40:29 -0400

    fix climb height with new climbers

--- src/main/java/frc/robot/climber/InclinatorHw.java
+++ src/main/java/frc/robot/climber/InclinatorHw.java
@@ -141,7 +141,7 @@ public class InclinatorHw implements IInclinatorHw {
         } else {
             rightLimit = false;
         }
-        rightHeight = 23.5 + (rightClimb.getSelectedSensorPosition() / COUNTS_PER_INCH);
+        rightHeight = 22.0 + (rightClimb.getSelectedSensorPosition() / COUNTS_PER_INCH);
         SmartDashboard.putNumber("Right Climber Height", rightHeight);
     }
 
